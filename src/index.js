6047bd5bc1e1a949430fd04f3a6a67746acd16f2
Andrea Brezanova <55324009+Andrea-Brezanova@users.noreply.github.com>
2024-12-21 12:26:23 +0100

    Injected more weather data

--- src/index.js
+++ src/index.js
@@ -4,9 +4,34 @@ function refreshWeather(response) {
     let temperatureElement = document.querySelector("#temperature");
     let temperature = Math.round(response.data.temperature.current);
     let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    let timeElement = document.querySelector("#time");
+    let dateElement = new Date(response.data.time * 1000); // convert to milliseconds
+
+    console.log(response.data.condition.description);
 
     cityElement.innerHTML = response.data.city;
+    timeElement.innerHTML = formatDate(dateElement);
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windElement.innerHTML = `${response.data.wind.speed}km/h`;
     temperatureElement.innerHTML = temperature;
+}
+
+function formatDate(date) {
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = ["Sunday", "Monday", "Tueday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    
+    let day = days[date.getDay()];
+
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
